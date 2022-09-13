86b7d8e21d693468d24b960915a6efc58a2f06ea
Caleb <caleb542@gmail.com>
2022-09-13 15:40:45 -0400

    starting a new recipe form

---
+++ src/addRecipe.js
@@ -0,0 +1,24 @@
+import "./style.scss";
+alert('hello')
+const newRecipe = {
+        name: "",
+        photoURL: "",
+        createdAt: "",
+        updatedAt: "",
+        author: "",
+        description: "",
+        categories: "",
+        article: "",
+        id: "",
+        ingredients: [{
+            name: "",
+            description: "",
+            amount: "",
+            unit: "",
+            measureWord: "",
+            alternatives: [],
+            id: ""
+        }],
+        "photographer": "",
+        "photographerLink": ""
+    }
